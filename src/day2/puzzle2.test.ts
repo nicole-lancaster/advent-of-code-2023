7dfa9fd9e38d9bc4cecb5c00e1d126e4f9805264
nicole <lancasternj@gmail.com>
2023-12-03 13:27:45 +0000

    parsing and formatting puzzle into a shape for solving

--- src/day2/puzzle2.test.ts
+++ src/day2/puzzle2.test.ts
@@ -4,5 +4,34 @@ describe("Day 2 parsePuzzleInput function", () => {
   test("should successfully read input file and parse data", () => {
     const input = "test-input.txt";
     const output = parsePuzzleInput(input);
+    expect(output).toEqual([
+      {
+        1: ['3 blue, 4 red',
+          '1 red, 2 green, 6 blue',
+          '2 green']
+      },
+      {
+        2: ['1 blue, 2 green',
+          '3 green, 4 blue, 1 red',
+          '1 green, 1 blue']
+      },
+      {
+        3: [
+          '8 green, 6 blue, 20 red',
+          '5 blue, 4 red, 13 green',
+          '5 green, 1 red'
+        ]
+      },
+      {
+        4: [
+          '1 green, 3 red, 6 blue',
+          '3 green, 6 red',
+          '3 green, 15 blue, 14 red'
+        ]
+      },
+      {
+        5: ['6 red, 1 blue, 3 green',
+          '2 blue, 1 red, 2 green']
+      }])
   });
 });
--- src/day2/puzzle2.ts
+++ src/day2/puzzle2.ts
@@ -1,7 +1,28 @@
 import fs from "fs"
 
+type GameSetsArray = string[][]
+type GameObjType = {
+  [key: number]: GameSetsArray;
+};
+
 export const parsePuzzleInput = (inputFile: string) => {
+
+  let gameObjArray: GameObjType[] = []
+  let gameObj = {}
+
   const inputData = fs
     .readFileSync(`${__dirname}/${inputFile}`, "utf-8")
     .toString()
+
+  const gamesArray: string[] = inputData.split("\n")
+  const removingGameWord = gamesArray.map((game) => {
+    return game.replaceAll("Game ", "")
+  })
+  const formattingGame = removingGameWord.map((game) => {
+    game.split(": ", game.length - 1)
+    const gameSets = game.split(": ")
+    const arrayOfEachGameSet = gameSets[1].split("; ")
+    return gameObj = { [game[0]]: arrayOfEachGameSet }
+  })
+  return formattingGame
 }
