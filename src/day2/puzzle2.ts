45bc120714e5148f8302657f6ffdef3d6964b2f0
nicole <lancasternj@gmail.com>
2023-12-03 14:00:59 +0000

    refactoring

--- src/day2/puzzle2.ts
+++ src/day2/puzzle2.ts
@@ -1,28 +1,26 @@
 import fs from "fs"
 
-type GameSetsArray = string[][]
+type GameSetsArray = string[]
 type GameObjType = {
-  [key: number]: GameSetsArray;
+  [key: string]: GameSetsArray;
 };
 
-export const parsePuzzleInput = (inputFile: string) => {
+export const parsePuzzleInput = (inputFile: string): GameObjType[] => {
 
-  let gameObjArray: GameObjType[] = []
-  let gameObj = {}
+  let gameObj: GameObjType = {}
 
   const inputData = fs
     .readFileSync(`${__dirname}/${inputFile}`, "utf-8")
     .toString()
 
   const gamesArray: string[] = inputData.split("\n")
-  const removingGameWord = gamesArray.map((game) => {
+  const removedGameWord: string[] = gamesArray.map((game) => {
     return game.replaceAll("Game ", "")
   })
-  const formattingGame = removingGameWord.map((game) => {
-    game.split(": ", game.length - 1)
-    const gameSets = game.split(": ")
-    const arrayOfEachGameSet = gameSets[1].split("; ")
-    return gameObj = { [game[0]]: arrayOfEachGameSet }
+  const formattedGames = removedGameWord.map((game) => {
+    const splitGameIdAndSets = game.split(": ")
+    const gameSetsArray: GameSetsArray = splitGameIdAndSets[1].split("; ")
+    return gameObj = { [game[0]]: gameSetsArray }
   })
-  return formattingGame
+  return formattedGames
 }
